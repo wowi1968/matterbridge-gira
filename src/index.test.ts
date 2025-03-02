0fd59e789370ee076fabbaafa7f324a9cbf84b2f
Wolfgang Willinghöfer <wolfgang@willinghoefer-net.de>
2025-03-02 16:15:35 +0100

    fix plugin test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import { Matterbridge, PlatformConfig } from 'matterbridge';
 import { AnsiLogger } from 'matterbridge/logger';
-import { ExampleMatterbridgeDynamicPlatform } from './platform.js';
+import { MatterbridgeGiraIotRestApi } from './platformGiraIotRestApi.js';
 import initializePlugin from './index';
 import { jest } from '@jest/globals';
 
@@ -20,16 +20,16 @@ describe('initializePlugin', () => {
     } as unknown as Matterbridge;
     mockLog = { fatal: jest.fn(), error: jest.fn(), warn: jest.fn(), notice: jest.fn(), info: jest.fn(), debug: jest.fn() } as unknown as AnsiLogger;
     mockConfig = {
-      'name': 'matterbridge-example-dynamic-platform',
+      'name': 'matterbridge-gira',
       'type': 'DynamicPlatform',
       'unregisterOnShutdown': false,
       'debug': false,
     } as PlatformConfig;
   });
 
-  it('should return an instance of TestPlatform', () => {
+  it('should return an instance of MatterbridgeGiraIotRestApi', () => {
     const result = initializePlugin(mockMatterbridge, mockLog, mockConfig);
 
-    expect(result).toBeInstanceOf(ExampleMatterbridgeDynamicPlatform);
+    expect(result).toBeInstanceOf(MatterbridgeGiraIotRestApi);
   });
 });
